556af6b8a52569f608968751b1cc25b20017af63
Andres Heinloo <andres@gfz-potsdam.de>
2022-11-16 15:02:00 +0100

    remove unfinished SMS component

--- luci-app-seiscomp/luasrc/controller/seiscomp/sms.lua
+++
@@ -1,9 +0,0 @@
--- (C) 2019 Andres Heinloo, Helmholtz-Zentrum Potsdam - Deutsches GeoForschungsZentrum GFZ
-
-module("luci.controller.seiscomp.sms", package.seeall)
-
-function index()
-	entry({"admin", "status", "sms"}, template("seiscomp/sms"), "SMS", 920)
-	entry({"admin", "services", "sms"}, cbi("seiscomp/sms"), "SMS", 920)
-end
-
